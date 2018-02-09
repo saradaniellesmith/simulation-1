a6c82c51907ead8c123507c2a1a7e820f535c5da
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-09 15:36:46 -0600

    header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import './Header.css';
+import Logo from './Shelfieimg.png';
 
 
-
-const Header = () => {
-  render()
+class Header extends Component {
+  render() {
     return(
     <div className = "header">
         <div className= "header-content">
-        <img src="" alt="logo"> </img>
+        <img src={Logo} alt="logo" /> 
         <span> SHELFIE </span>
         </div>
     </div>
     )
+  }
 } 
 
 export default Header;
